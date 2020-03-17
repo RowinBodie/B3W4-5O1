d5258f622d2483b298c906ffdc07d811fcb451be
rowin <robogom24@gmail.com>
2020-03-17 12:36:36 +0100

    Files

---
+++ B3W4O1 - Dynamische Applicatie/files/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/map.css">
+    <title>Characters</title>
+</head>
+<body>
+    <div id="paginaBreedte">
+        <h1>Alle 10 characters uit database</h1>
+    <div>
+</body>
+</html>
