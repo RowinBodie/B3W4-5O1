212617b75ee7519aada42134c0cb16b5c77c6542
rowin <robogom24@gmail.com>
2020-03-18 11:46:46 +0100

    sql gelinkt

--- B3W4O1 - Dynamische Applicatie/files/index.php
+++ B3W4O1 - Dynamische Applicatie/files/index.php
@@ -1,14 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "pizzarol";
+$password = "ccO6tMXHgyLsywsz";
+$dbname = "characters";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=characters", $username, $password);
+    // set the PDO error mode to exception
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+    $stmt = $conn->prepare("SELECT id, name, avatar, health, attack, defense  FROM characters;");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/map.css">
-    <title>Characters</title>
+    <title>Document</title>
 </head>
 <body>
-    <div id="paginaBreedte">
-        <h1>Alle 10 characters uit database</h1>
-    <div>
+    <img src="../avatars/images/<?=$result['0']['avatar']?>"></img>
 </body>
 </html>
